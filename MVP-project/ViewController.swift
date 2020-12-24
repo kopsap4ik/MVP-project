7088ac61c581e922e77df4847c6359db6794acb4
Vasily Petuhov <vspetukhov@icloud.com>
2020-12-24 13:49:50 +0300

    Initial Commit

---
+++ MVP-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-project
+//
+//  Created by Василий Петухов on 24.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVP-projectTests/MVP_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  MVP_projectTests.swift
+//  MVP-projectTests
+//
+//  Created by Василий Петухов on 24.12.2020.
+//
+
+import XCTest
+@testable import MVP_project
+
+class MVP_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
